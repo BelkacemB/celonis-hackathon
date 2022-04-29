2360768a29b4f839c41a3f897e8a719577648914
Belkacem Berchiche <b_berchiche@esi.dz>
2022-04-29 19:32:42 +0200

    Use .env file for token & URL

--- clusterize.py
+++ clusterize.py
@@ -1,2 +1,8 @@
 from pycelonis import get_celonis
-celonis = get_celonis()
+import os
+from dotenv import load_dotenv
+
+celonis = get_celonis(
+    url=os.getenv("URL"),
+    api_token=os.getenv("API_TOKEN")
+)
